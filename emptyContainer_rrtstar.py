b759456edb6cc0c5388a97cfe2afd12f230b8c15
Arti Anantharaman <artianantharaman@gmail.com>
2020-04-19 15:40:17 -0400

    resetting with get_graspable_objects

--- emptyContainer_rrtstar.py
+++ emptyContainer_rrtstar.py
@@ -73,6 +73,33 @@ class NoisyObjectPoseSensor:
 
         return obj_poses
 
+class GraspableObjectPoseSensor:
+
+    def __init__(self, env):
+        self._env = env
+
+        self._pos_scale = [0.005] * 3
+        self._rot_scale = [0.01] * 3
+
+    def get_poses(self):
+        objs = self._env._scene._active_task.get_graspable_objects()
+        obj_poses = {}
+
+        for obj in objs:
+            name = obj.get_name()
+            pose = obj.get_pose()
+
+            pos, quat_wxyz = sample_normal_pose(self._pos_scale, self._rot_scale)
+            gt_quat_wxyz = quaternion(pose[6], pose[3], pose[4], pose[5])
+            perturbed_quat_wxyz = quat_wxyz * gt_quat_wxyz
+
+            pose[:3] += pos
+            pose[3:] = [perturbed_quat_wxyz.x, perturbed_quat_wxyz.y, perturbed_quat_wxyz.z, perturbed_quat_wxyz.w]
+
+            obj_poses[name] = pose
+
+        return obj_poses
+
 def move_to_location(task, obs, desired_pose, obj_pose_sensor, tolerance=0.5, isopen=1):
 	while True:
 		obj_poses = obj_pose_sensor.get_poses()
@@ -93,11 +120,11 @@ def move_rrt(task, source, dest, open=1):
 		obs, reward, terminate = task.step(p.tolist()+[open])
 	return obs, reward, terminate
 
-def check_shape_loc(obs, shape_ind):
-	objPoses = obj_pose_sensor.get_poses()
+def check_shape_loc(graspable_pose_sensor, obs, shape_ind):
+	objPoses = graspable_pose_sensor.get_poses()
 	
 	if shape_ind == 0:
-		upright_s0 = objPoses["Shape0"]
+		upright_s0 = objPoses["Shape"]
 		upright_s0[3:] = obs.gripper_pose[3:]
 		return upright_s0
 
@@ -111,7 +138,20 @@ def check_shape_loc(obs, shape_ind):
 		upright_s3[3:] = obs.gripper_pose[3:]
 		return upright_s3
 
+def test_if_grasped(obs, objs, objname):
+	for obj in objs:
+		name = obj.get_name()
 
+		pose = obj.get_pose()
+		print("name and pose", name, pose)
+
+		if(name==objname):
+			err = np.linalg.norm(obs.gripper_pose[0:3]-pose[0:3])
+			print('Err = ', err)
+			if err > 0.01:
+				print('Shape not picked up')
+				return False
+	return True
 
 
 if __name__ == "__main__":
@@ -120,6 +160,7 @@ if __name__ == "__main__":
 	task = env.get_task(EmptyContainer) # available tasks: EmptyContainer, PlayJenga, PutGroceriesInCupboard, SetTheTable
 	agent = RandomAgent()
 	obj_pose_sensor = NoisyObjectPoseSensor(env)
+	graspable_pose_sensor = GraspableObjectPoseSensor(env)
 
 	descriptions, obs = task.reset()
 	print(descriptions)
@@ -148,10 +189,17 @@ if __name__ == "__main__":
 		
 	print("Forward pass begins:")
 	for i in [0, 1, 3]:
-		shape_loc = check_shape_loc(obs, i)
-		obs, reward, terminate = move_rrt(task, waypoint2, shape_loc)
-		obs, reward, terminate = task.step((obs.gripper_pose).tolist()+[0])
-		obs, reward, terminate = move_rrt(task, shape_loc, waypoint2, 0)
+		objname = "Shape"+str(i)
+		if i==0:
+			objname = "Shape"
+		shape_loc = check_shape_loc(graspable_pose_sensor, obs, i)
+		objs = env._scene._active_task.get_graspable_objects()
+
+		while not test_if_grasped(obs, objs, objname):
+			obs, reward, terminate = move_rrt(task, waypoint2, shape_loc)
+			obs, reward, terminate = task.step((obs.gripper_pose).tolist()+[0])
+			obs, reward, terminate = move_rrt(task, shape_loc, waypoint2, 0)
+
 		obs, reward, terminate = move_rrt(task, waypoint2, waypoint3, 0)
 		droploc = waypoint3.copy()
 		droploc[2]-=0.1
@@ -183,7 +231,7 @@ if __name__ == "__main__":
 
 
 	for i in [0, 1, 3]:
-		shape_loc = check_shape_loc(obs, i)
+		shape_loc = check_shape_loc(graspable_pose_sensor, obs, i)
 		obs, reward, terminate = move_rrt(task, waypoint2, waypoint3)
 		obs, reward, terminate = move_rrt(task, waypoint3, shape_loc)
 		obs, reward, terminate = task.step((obs.gripper_pose).tolist()+[0])
